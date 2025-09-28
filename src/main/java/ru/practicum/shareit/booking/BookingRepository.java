2b3eaf38c913cee196019e811338c5ac23aeae59
Evgeniya <eva.sharapova1@mail.ru>
2025-09-28 12:09:30 +0300

    V1_3

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -7,11 +7,15 @@ import java.time.Instant;
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
     @Query(value = "select b from bookings as b" +
             "join items as it on it.id = b.item_id " +
             "join users as us on us.id = it.owner_id " +
             "where us.id = ?1", nativeQuery = true)
+
     List<Booking> findByOwner_IdOrderByStartDesc(Long ownerId);
+    
     List<Booking> findByBooker_IdOrderByStartDesc(Long bookerId);
-    List<Booking> findByBooker_IdAndItem_IdAndEndIsBefore(Long bookerId, Long ItemId, Instant end);
+
+    List<Booking> findByBooker_IdAndItem_IdAndEndIsBefore(Long bookerId, Long itemId, Instant end);
 }
