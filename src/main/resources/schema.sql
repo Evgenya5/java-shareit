4beb975f9684fd49d8d21891bf939b58fb8342ee
Evgeniya <eva.sharapova1@mail.ru>
2025-09-29 12:08:36 +0300

    V1_8

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,8 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
               id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
               email varchar(100) NOT NULL,
               name varchar(200),
-              CONSTRAINT users_pk PRIMARY KEY (id),
-              CONSTRAINT email_uk UNIQUE (email)
+              UNIQUE (email)
           );
 CREATE TABLE IF NOT EXISTS items (
               id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -11,16 +10,14 @@ CREATE TABLE IF NOT EXISTS items (
               name varchar(100) NOT NULL,
               is_available BOOLEAN DEFAULT TRUE,
               owner_id BIGINT REFERENCES users (id) ON DELETE CASCADE,
-              request_id varchar(200),
-              CONSTRAINT items_pk PRIMARY KEY (id)
+              request_id varchar(200)
           );
 CREATE TABLE IF NOT EXISTS comments (
               id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
               text varchar(200),
               created date,
               author_id BIGINT REFERENCES users (id) ON DELETE CASCADE,
-              item_id BIGINT REFERENCES items (id) ON DELETE CASCADE,
-              CONSTRAINT comments_pk PRIMARY KEY (id)
+              item_id BIGINT REFERENCES items (id) ON DELETE CASCADE
           );
 CREATE TABLE IF NOT EXISTS bookings (
               id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -30,6 +27,5 @@ CREATE TABLE IF NOT EXISTS bookings (
               status varchar(100) NOT NULL,
               is_available BOOLEAN DEFAULT TRUE,
               item_id BIGINT REFERENCES items (id) ON DELETE CASCADE,
-              booker_id BIGINT REFERENCES users (id) ON DELETE CASCADE,
-              CONSTRAINT bookings_pk PRIMARY KEY (id)
+              booker_id BIGINT REFERENCES users (id) ON DELETE CASCADE
           );
