9de578ee59b013b97a14892274582563cee064ef
Evgeniya <eva.sharapova1@mail.ru>
2025-10-14 13:11:59 +0300

    пробуем добавлять тесты

---
+++ server/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,26 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
